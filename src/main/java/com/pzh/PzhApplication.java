246c1be627c5791b7e37898e3ce64bfd0e3d8bbc
Haoting Qin <1165101405@qq.com>
2022-06-26 21:11:57 +0800

    增加了扫描注解

--- src/main/java/com/pzh/PzhApplication.java
+++ src/main/java/com/pzh/PzhApplication.java
@@ -3,7 +3,8 @@ package com.pzh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {"com.pzh"})
 public class PzhApplication {
 
 	public static void main(String[] args) {
