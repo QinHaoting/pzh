07532443fef7bbc267e9a9537043d7e538b673da
Haoting Qin <1165101405@qq.com>
2022-06-27 09:40:54 +0800

    添加了了公司的日期字段

--- src/main/java/com/pzh/domain/Firm.java
+++ src/main/java/com/pzh/domain/Firm.java
@@ -4,11 +4,13 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @ApiModel(value="Firm公司实体")
 @Data
@@ -37,4 +39,9 @@ public class Firm implements Serializable {
     @ApiModelProperty(value="联系电话", name="phone", required=true, example = "10086")
     @TableField(value = "f_phone")
     private String phone;
+
+    @ApiModelProperty(value="创办时间", name="date", required=true, example = "10086")
+    @TableField(value = "f_date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    private Date date;
 }
