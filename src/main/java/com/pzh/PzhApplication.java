ed2265ffca819fbdcd1ab72c45f8db1a1e47cc5a
Haoting <1165101405@qq.com>
2025-05-01 10:24:01 +0800

    1. 重新配置DB连接池 2. 配置redis 3. 配置分布式session 4. 配置日志

--- src/main/java/com/pzh/PzhApplication.java
+++ src/main/java/com/pzh/PzhApplication.java
@@ -2,10 +2,12 @@ package com.pzh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication(scanBasePackages = {"com.pzh"})
+@EnableCaching
 public class PzhApplication {
 
 	public static void main(String[] args) {
